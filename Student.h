b948ae970730f02c5b9647a532af3162b3998807
李世达 <lisd@6renyou.com>
2018-12-07 17:51:04 +0800

    第一次版本提交

---
+++ Student.h
@@ -0,0 +1,17 @@
+//
+//  Student.h
+//  Demo_pay
+//
+//  Created by 李世达 on 2018/12/7.
+//  Copyright © 2018 李世达. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Student : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Student.m
@@ -0,0 +1,23 @@
+//
+//  Student.m
+//  Demo_pay
+//
+//  Created by 李世达 on 2018/12/7.
+//  Copyright © 2018 李世达. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        NSLog(@"我是一个学生");
+    }
+    return self;
+}
+
+@end
